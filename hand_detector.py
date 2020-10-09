98e348cc77a1ee5a1e48aedc6b416ce907c4a611
Aleksandr Yeganov <ayeganov@cisco.com>
2020-10-09 12:46:04 -0400

    Update hand detector to extract hands

--- hand_detector.py
+++ hand_detector.py
@@ -116,32 +116,54 @@ class SkinDetector:
         self._top_sample = None
         self._bottom_sample = None
 
+        self.selection = None
+        self.drag_start = None
+        self.track_window = None
+        self.hist = None
+
     @property
     def calibrated(self):
-        return self._hue_low is not None
+        return self.hist is not None and self.track_window is not None
 
-    def draw_skin_samples(self, image):
+    def sample_skin_color(self, vis, hsv, mask):
         """
-        Draw the skin sample rectangles
+        Samples the color of the skin, and returns the image with the
+        highlighted area of selection
 
         @param image - numpy array representing the image
-        @returns two tuples of bottom left corners of the skin samples
+        @param hsv - image converted to hsv color space
+        @param mask - image with background removed
+        @returns 
         """
-        color_bgr = (0, 0, 255)
-        x, y = 150, 50
+        if self.selection is None:
+            return vis
 
-        draw_rectangle(image, x, y, 160, 380, color_bgr)
+        if self.track_window is not None:
+            return vis
 
-        x, y = 200, 100
-        width = 60
-        height = 80
-        color_bgr = (0, 255, 0)
+        x0, y0, x1, y1 = self.selection
+        hsv_roi = hsv[y0:y1, x0:x1]
+        mask_roi = mask[y0:y1, x0:x1]
+        hist = cv2.calcHist( [hsv_roi], [0], mask_roi, [16], [0, 180] )
+        cv2.normalize(hist, hist, 0, 255, cv2.NORM_MINMAX)
+        self.hist = hist.reshape(-1)
+        self.show_hist()
 
-        self._top_sample = draw_rectangle(image, x, y, width, height, color_bgr)
-        y = 300
-        self._bottom_sample = draw_rectangle(image, x, y, width, height, color_bgr)
+        vis_roi = vis[y0:y1, x0:x1]
+        cv2.bitwise_not(vis_roi, vis_roi)
+        vis[mask == 0] = 0
 
-        return image, self._top_sample, self._bottom_sample
+        return vis
+
+    def show_hist(self):
+        bin_count = self.hist.shape[0]
+        bin_w = 24
+        img = np.zeros((256, bin_count*bin_w, 3), np.uint8)
+        for i in range(bin_count):
+            h = int(self.hist[i])
+            cv2.rectangle(img, (i*bin_w+2, 255), ((i+1)*bin_w-2, 255-h), (int(180.0*i/bin_count), 255, 255), -1)
+        img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
+        cv2.imshow('hist', img)
 
     def calibrate(self, image):
         image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -191,6 +213,72 @@ class SkinDetector:
         output = cv2.dilate(output, struct_element, iterations=1)
 
         return output
+
+    def on_mouse(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.drag_start = (x, y)
+            self.track_window = None
+        if self.drag_start:
+            xmin = min(x, self.drag_start[0])
+            ymin = min(y, self.drag_start[1])
+            xmax = max(x, self.drag_start[0])
+            ymax = max(y, self.drag_start[1])
+            self.selection = (xmin, ymin, xmax, ymax)
+        if event == cv2.EVENT_LBUTTONUP:
+            self.drag_start = None
+            self.track_window = (xmin, ymin, xmax - xmin, ymax - ymin)
+
+    def extract_hand_image(self, hsv, mask):
+        """
+        Calculates the location of the hand in the image
+
+        @param hsv - frame in HSV color space
+        @param mask - best guess at background removal
+        """
+        prob = cv2.calcBackProject([hsv], [0], self.hist, [0, 180], 1)
+        prob &= mask
+        term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
+        track_box, self.track_window = cv2.CamShift(prob, self.track_window, term_crit)
+
+        change_me = prob.copy()
+        change_me[change_me > 0] = 255
+
+        kernel = np.ones((5, 5), np.uint8)
+        change_me = cv2.morphologyEx(change_me, cv2.MORPH_CLOSE, kernel)
+
+        image, contours, hierarchy = cv2.findContours(change_me, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours_poly = [None] * len(contours)
+        bound_rect = [None] * len(contours)
+
+        for i, c in enumerate(contours):
+            contours_poly[i] = cv2.approxPolyDP(c, 3, True)
+            bound_rect[i] = cv2.boundingRect(contours_poly[i])
+
+        try:
+            hand_box = None
+            max_area = 0
+            for i in range(len(contours)):
+                x, y, w, h = np.array(bound_rect[i]).astype(np.uint16)
+    #                        print(f"Drawing bounding rect {int_rect}")
+                pt1, pt2 = (x, y), (x+w, y+h)
+                area = w * h
+                if area > max_area:
+                    max_area = area
+                    hand_box = pt1, pt2
+
+            if max_area < 1000:
+                return None, change_me, None
+
+            pt1, pt2 = hand_box
+            hand = change_me[pt1[0]:pt2[0], pt1[1]:pt2[1]]
+
+            return hand, change_me, hand_box
+
+        except Exception as error:
+            traceback.print_exc()
+            print(error)
+            print(track_box)
+            raise RuntimeError("Failed to extract image")
 
 
 def draw_skin_status(image, skin_detector):
@@ -247,7 +335,7 @@ def draw_process_status(image, mode):
 
 
 def draw_message(image, msg):
-    location = (300, 700)
+    location = (100, 700)
     font_scale = 1
     color = (250, 0, 5)
     thickness = 2
@@ -283,16 +371,17 @@ def mouse_callback(event, x, y, event_flag, skin_detector):
     print(f"event: {event}, x: {x}, y: {y}, flag: {event_flag}, detector: {skin_detector}")
 
 
-def start_capture(resolution, model_path):
+def start_capture(resolution, output_res, model_path):
     cam = cv2.VideoCapture(0)
 
     if not cam.isOpened():
         print("Failed to open the camera, bailing...")
         return
 
-    cam.set(3, 1280)
-    cam.set(4, 720)
-
+    print(f"Starting the camera in resolution: {resolution}")
+    width, height = resolution
+    cam.set(3, width)
+    cam.set(4, height)
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(f"Using '{device}' for running the model")
@@ -303,7 +392,7 @@ def start_capture(resolution, model_path):
     skin_status_location = (20, 20)
 
     cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
-    cv2.setMouseCallback(WINDOW_NAME, mouse_callback, param=detector)
+    cv2.setMouseCallback(WINDOW_NAME, detector.on_mouse)
     cv2.createTrackbar("Vmin", WINDOW_NAME, 30, 255, lambda _: None)
     cv2.createTrackbar("Vmax", WINDOW_NAME, 255, 255, lambda _: None)
     cv2.createTrackbar("Smin", WINDOW_NAME, 30, 255, lambda _: None)
@@ -311,8 +400,11 @@ def start_capture(resolution, model_path):
     mask = None
     mode = "setup"
     class_names = ["closed", "open"]
+    show_backproj = False
 
-    message = "Press 'c' for skin calibration, 'b' for background"
+    message = "Use mouse for skin calibration"
+    location = None
+    index = 0
 
     while cam.isOpened():
         try:
@@ -324,15 +416,20 @@ def start_capture(resolution, model_path):
                 print("failed to grab frame")
                 break
 
+            if k == ord('r'):
+                show_backproj = not show_backproj
             if k == ord('o'):
                 # Open pressed
                 print("OPEN")
+                location = "open"
+                message = f"Collecting data for {location} hand"
+            elif k == ord('c'):
+                print("CLOSED")
+                location = "closed"
+                message = f"Collecting data for {location} hand"
             elif k == ord('q'):
                 print("Exiting by user request...")
                 break
-            elif k == ord('c'):
-                print("Performing calibration...")
-                detector.calibrate(frame)
             elif k == ord('m'):
                 print("Calculating the skin mask")
                 foreground = bg_remover.get_foreground(frame)
@@ -343,76 +440,69 @@ def start_capture(resolution, model_path):
             elif k == ord('f'):
                 bg_remover.get_foreground(frame)
             elif k == ord('p'):
-                if not (detector.calibrated and bg_remover.calibrated):
-                    message = "Calibrate skin detector and bg remover"
+                if not detector.calibrated:
+                    message = "Calibrate skin detector"
                     continue
                 mode = "process"
-                message = "Move your hand into the highlighted square"
+                message = "Move your hand"
             elif k == ord('s'):
                 mode = "setup"
-                message = "Press 's' for skin calibration, 'b' for background"
+                message = "Use mouse for skin calibration"
             elif k == ord('d'):
-                if not (detector.calibrated and bg_remover.calibrated):
-                    message = "Calibrate skin detector and bg remover"
-                    continue
-                mode = "data"
-                message = "Move your hand into the highlighted square"
+                location = None
+                message = "Move your hand"
             elif k == 27:
                 # ESC pressed
                 print("Escape hit, closing...")
                 break
 
             draw_on_me = frame.copy()
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            mask = cv2.inRange(hsv, np.array((0., 60., 32.)), np.array((180., 255., 255.)))
 
             draw_message(draw_on_me, message)
             if mode == "setup":
-                skin_samples, top, bottom = detector.draw_skin_samples(draw_on_me)
-                draw_process_status(skin_samples, mode)
+                draw_on_me = detector.sample_skin_color(draw_on_me, hsv, mask)
+                draw_process_status(draw_on_me, mode)
 
-                (x_left, y_left), (x_right, y_right) = top
-                top_sample = frame[y_left:y_right, x_left:x_right]
+                draw_skin_status(draw_on_me, detector)
 
-                (x_left, y_left), (x_right, y_right) = bottom
-                bottom_sample = frame[y_left:y_right, x_left:x_right]
-
-                draw_skin_status(skin_samples, detector)
-                draw_bg_status(skin_samples, bg_remover)
-
-                cv2.imshow(WINDOW_NAME, skin_samples)
-
-
-    #            cv2.imshow("top_sample", top_sample)
-    #            cv2.imshow("bottom_sample", bottom_sample)
+                cv2.imshow(WINDOW_NAME, draw_on_me)
 
             elif mode == "process":
-                foreground = bg_remover.get_foreground(frame)
-                mask = detector.calc_skin_mask(foreground)
+                hand, back_proj, coordinates = detector.extract_hand_image(hsv, mask)
 
-                (x_left, y_left), (x_right, y_right) = draw_hand_square(draw_on_me)
+                if hand is None:
+                    print("failed to find hand")
+                    continue
 
-                hand = mask[y_left:y_right, x_left:x_right]
-                hand = cv2.bitwise_not(hand)
+                pt1, pt2 = coordinates
+                center = int((pt1[0] + pt2[0]) / 2), int((pt1[1] + pt2[1]) / 2)
+#                print(f"Center: {center}")
 
-#                kernel = np.ones((5,5),np.uint8)
-#                hand = cv2.morphologyEx(hand, cv2.MORPH_CLOSE, kernel)
+                if show_backproj:
+                    draw_on_me[:] = back_proj[...,np.newaxis]
 
-                rgb_hand = cv2.cvtColor(hand, cv2.COLOR_GRAY2RGB)
-                draw_on_me[y_left:y_right, x_left:x_right] = cv2.cvtColor(hand, cv2.COLOR_GRAY2RGB)
+                draw_on_me = cv2.rectangle(draw_on_me, pt1, pt2, (0, 255, 0), 2)
+                draw_on_me = cv2.circle(draw_on_me, center, 5, (0, 255, 200), 2, cv2.LINE_AA)
 
-                net_input = eval_transform(rgb_hand).to(device)
-
-                batch = net_input[None, ...]
-
-                outputs = network(batch)
-                _, preds = torch.max(outputs, 1)
-
-                hand_state = class_names[preds[0]]
-                print(f"hand_state: {hand_state}")
-
-                draw_process_status(draw_on_me, mode)
                 cv2.imshow(WINDOW_NAME, draw_on_me)
-#                if mask is not None:
-#                    cv2.imshow(WINDOW_NAME, mask)
+
+                if location is not None:
+                    frame_to_size = cv2.resize(hand, output_res)
+
+                    prefix = time.time_ns()
+                    file_name = f"{prefix}{str(index)}.png"
+                    full_path = os.path.join(location, file_name)
+
+                    if not os.path.exists(location):
+                        try:
+                            os.makedirs(location)
+                        except FileExistsError:
+                            pass
+                    cv2.imwrite(full_path, frame_to_size)
+                    print(full_path)
+                    index = index + 1
 
         except KeyboardInterrupt:
             # When everything done, release the capture
@@ -439,7 +529,11 @@ def main():
     parser = argparse.ArgumentParser(description="Capture Images to specified folder at maximum rate")
     parser.add_argument("-r",
                         "--resolution",
-                        default="50x50",
+                        default="1024x768",
+                        type=to_resolution)
+    parser.add_argument("-o",
+                        "--output_res",
+                        default="120x120",
                         type=to_resolution)
     parser.add_argument("-m",
                         "--model",
@@ -448,7 +542,7 @@ def main():
 
     args = parser.parse_args()
 
-    start_capture(args.resolution, args.model.name)
+    start_capture(args.resolution, args.output_res, args.model.name)
 
 
 if __name__ == "__main__":
